28fffca78b57eb2df5a0562a41dcc7fecefc05f9
Wilfred Hughes <me@wilfred.me.uk>
2018-10-15 01:01:52 +0100

    Initial skeleton

---
+++ ertcheck.el
@@ -0,0 +1,35 @@
+;;; ertcheck.el --- quickcheck/hypothesis style ert tests         -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Wilfred Hughes
+
+;; Author: Wilfred Hughes <me@wilfred.me.uk>
+;; Keywords: testing
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; ERT extensions to bring property-based testing to elisp. Heavily
+;; influenced by the wonderful Hypothesis project.
+
+;; References:
+
+;; https://hypothesis.works/articles/how-hypothesis-works/
+;; https://hypothesis.readthedocs.io/en/latest/details.html#how-good-is-assume
+;; https://github.com/HypothesisWorks/hypothesis/blob/master/guides/internals.rst
+
+;;; Code:
+
+(provide 'ertcheck)
+;;; ertcheck.el ends here
