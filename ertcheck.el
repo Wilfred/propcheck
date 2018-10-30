2d5132aba1de34914597c6cd76b748439d657091
Wilfred Hughes <me@wilfred.me.uk>
2018-10-30 09:13:36 +0000

    Prefer longer strings/lists in test generation
    Previously we generated empty values very often.

--- ertcheck.el
+++ ertcheck.el
@@ -84,9 +84,9 @@ Note that elisp does not have a separate character type."
 (defun ertcheck-generate-list (testdata item-generator)
   "Generate a list whose items are drawn from ITEM-GENERATOR."
   (let ((result nil))
-    ;; Dumb: 50% chance of making the list bigger on each draw.
+    ;; Dumb: 75% chance of making the list bigger on each draw.
     ;; See utils.py/more in Hypothesis for a smarter approach.
-    (while (>= (car (ertcheck-draw-bytes testdata 1)) 128)
+    (while (>= (car (ertcheck-draw-bytes testdata 1)) 64)
       (push (funcall item-generator testdata) result))
     result))
 
@@ -98,10 +98,10 @@ Note that elisp does not have a separate character type."
 (defun ertcheck-generate-string (testdata)
   "Generate a string using TESTDATA."
   (let ((chars nil))
-    ;; Dumb: 50% chance of making the string bigger on each draw.
+    ;; Dumb: 75% chance of making the string bigger on each draw.
     ;; TODO: see what hypothesis does
     ;; TODO: multibyte support, key sequence support
-    (while (>= (car (ertcheck-draw-bytes testdata 1)) 128)
+    (while (>= (car (ertcheck-draw-bytes testdata 1)) 64)
       (push
        (ertcheck-generate-ascii-char testdata)
        chars))
