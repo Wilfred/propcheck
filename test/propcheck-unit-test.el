795f1941b722956a75d3bacb2d8c1e706b86d76b
Wilfred Hughes <me@wilfred.me.uk>
2019-08-04 13:46:44 +0100

    Add test for drawing existing bytes from a seed

--- test/propcheck-unit-test.el
+++ test/propcheck-unit-test.el
@@ -7,3 +7,12 @@
      (= (length bytes) 10))
     (should
      (= (propcheck-seed-i seed) 10))))
+
+(ert-deftest propcheck--draw-bytes--replay ()
+  (let* ((propcheck--shrinks-remaining 999)
+         (seed (propcheck-seed '(1 2 3 4)))
+         (bytes (propcheck--draw-bytes seed 4)))
+    (should
+     (equal bytes '(1 2 3 4)))
+    (should
+     (= (propcheck-seed-i seed) 4))))
